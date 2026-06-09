4846c4a98737333530724ccc05cd37fcd4b5fdca
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-06-09 16:45:25 +0200

    fix(hashtable): fix issue when ht is full and no key match

--- src/ds/hashtable/get_pointer.c
+++ src/ds/hashtable/get_pointer.c
@@ -10,14 +10,18 @@ char *get_pointer(t_ht *ht, char *key)
 {
     size_t  n;
     size_t  index;
+    size_t  tmp_index;
 
     n = ft_strlen(key);
     index = joaat_hash((unsigned char *)key, n) % ht->capacity;
+    tmp_index = index - 1;
     while (ht->indexes[index].key != NULL)
     {
         if (ht->indexes[index].key != DELETED)
             if (ft_strncmp(ht->indexes[index].key, key, n + 1) == 0)
                 return ht->indexes[index].value;
+        if (__builtin_expect(tmp_index == index, 0))
+            return (NULL);
         index = (index + 1) % ht->capacity;
     }
     return (NULL);
