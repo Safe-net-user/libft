f0024338c9c521a19895081eba1a749408b044e0
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-06-15 12:13:48 +0200

    fix(string_builder): error name, variable set but don't used fixed

--- src/string/string_builder/append_ch_sb.c
+++ src/string/string_builder/append_ch_sb.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   append_char_sb.c                                   :+:      :+:    :+:   */
+/*   append_ch_sb.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gd-hallu <gd-hallu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/15 11:30:46 by gd-hallu          #+#    #+#             */
-/*   Updated: 2026/06/15 11:32:30 by gd-hallu         ###   ########.fr       */
+/*   Updated: 2026/06/15 12:12:21 by gd-hallu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,20 @@ static int extend_sb(t_sb *sb, size_t size_new_str)
     return (1);
 }
 
-int append_sb(t_sb *sb, char c)
+int append_ch_sb(t_sb *sb, char c)
 {
-    int  n;
+    size_t  n;
     size_t  pre_i;
-    size_t  final_i;
-    size_t  index;
 
     if (!sb || !c)
         return (0);
     n = 1;
-    index = 0;
     pre_i = sb->lenght;
     if (n > SIZE_MAX - pre_i)
         return (0);
     if (n + pre_i >= sb->capacity)
         if (extend_sb(sb, n) == 0)
             return (0);
-    final_i = pre_i + n;
     sb->str[pre_i++] = c;
     sb->lenght++;
     sb->str[pre_i] = '\0';
