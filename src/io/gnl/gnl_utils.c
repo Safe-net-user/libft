9a7faf350b2922abcc88e1e978b97f251ae36042
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-08-20 13:31:47 +0200

    quick push

--- src/io/gnl/gnl_utils.c
+++ src/io/gnl/gnl_utils.c
@@ -17,7 +17,7 @@ char    *gnl_strchr(const char *s, int c)
 {
     while (s && *s)
     {
-        if (s == (char)c)
+        if (*s == (char)c)
             return ((char *)s);
         s++;
     }
