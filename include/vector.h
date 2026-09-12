7deb57ae08ee09a4a7d37490cc9567fa7eb55079
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-12 20:30:30 +0200

    refactor(vector): change signature of init_vector

--- include/vector.h
+++ include/vector.h
@@ -12,7 +12,7 @@ typedef struct s_vector{
 
 }	t_vec;
 
-t_vec	*init_vector(void *type, size_t n);
+t_vec	*init_vector(int size_element, size_t n);
 size_t	size_vector(t_vec *v);
 void	push_back_vector(t_vec *v, void *x);
 void	pop_back_vector(t_vec *v);
--- src/ds/vector/init_vector.c
+++ src/ds/vector/init_vector.c
@@ -1,22 +1,22 @@
 #include "vector.h"
 #include <stdint.h>
 #include <stdlib.h>
-t_vec	*init_vector(void *type, size_t n)
+t_vec	*init_vector(int size_element, size_t n)
 {
 	t_vec	*v;
 
-	if (n == 0 || n > SIZE_MAX / sizeof(type))
+	if (n == 0 || n > SIZE_MAX / size_element)
 		return (NULL);
 	v = malloc(sizeof(t_vec));
 	if (!v)
 		return (NULL);
-	v->buffer = malloc(sizeof(type) * n);
+	v->buffer = malloc(size_element * n);
 	if (!v->buffer)
 	{
 		free(v);
 		return (NULL);
 	}
-	v->size_element = sizeof(*type);
+	v->size_element = size_element;
 	v->curr = v->buffer;
 	v->capacity = n;
 	v->size = 0;
