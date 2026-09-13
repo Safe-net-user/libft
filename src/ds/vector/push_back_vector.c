b1b5899873592ad8bc0b9e008cbc00c0fd0a6513
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-14 01:24:50 +0200

    fix(vector): misaligned occured a realloc fixed

--- src/ds/vector/push_back_vector.c
+++ src/ds/vector/push_back_vector.c
@@ -1,16 +1,24 @@
 #include "vector.h"
 #include "ft_memory.h"
 #include "ft_stdlib.h"
+#include<stddef.h>
 
 void	push_back_vector(t_vec *v, void *x)
 {
+	size_t	new_capacity;
+
 	if (!v || !x)
 		return ;
 	if (v->capacity == 0)
-		v->capacity++;
+		v->capacity = 1;
 	if (v->capacity == v->size)
-		v->buffer = ft_realloc(v->buffer, v->capacity * v->size_element, (v->capacity * v->size_element) * 3 / 2);
+	{
+		new_capacity = v->capacity * 3 / 2;
+		v->buffer = ft_realloc(v->buffer, v->capacity * v->size_element, new_capacity * v->size_element);
+		v->curr = (char *)v->buffer + v->size * v->size_element;
+		v->capacity = new_capacity;
+	}
 	ft_memcpy(v->curr, x, v->size_element);
-	v->curr += v->size_element;
+	v->curr = (char *)v->curr + v->size_element;
 	v->size++;
 }
