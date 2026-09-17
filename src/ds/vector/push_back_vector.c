cab5d60c88e706058b40c9c44d38430a92d828b8
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-17 21:26:03 +0200

    fix(vector): free vector if a bad alloc happended

--- src/ds/vector/push_back_vector.c
+++ src/ds/vector/push_back_vector.c
@@ -3,6 +3,8 @@
 #include "ft_stdlib.h"
 #include<stddef.h>
 
+#include "../../../include/vector.h"
+
 void	push_back_vector(t_vec *v, void *x)
 {
 	size_t	new_capacity;
@@ -15,6 +17,11 @@ void	push_back_vector(t_vec *v, void *x)
 	{
 		new_capacity = v->capacity * 3 / 2;
 		v->buffer = ft_realloc(v->buffer, v->capacity * v->size_element, new_capacity * v->size_element);
+		if (!v->buffer)
+		{
+			free_vector(v);
+			return ;
+		}
 		v->curr = (char *)v->buffer + v->size * v->size_element;
 		v->capacity = new_capacity;
 	}
